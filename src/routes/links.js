b30451d24d306bab6ce1b2dda67f58b740d0b6ce
Eslim Daga <esliim9811@gmail.com>
2021-01-24 11:07:19 -0500

    Add route get and insert link

--- src/routes/links.js
+++ src/routes/links.js
@@ -6,4 +6,11 @@ router.get("/add", (req,res) => {
   res.render("links/add");
 });
 
+router.post("/add", async(req,res) => {
+  const { title, url, description } = req.body;
+  const newLink = { title, url, description };
+  await pool.query("INSERT INTO links SET ?", [newLink]);
+  res.send("Received");
+});
+
 module.exports = router;
