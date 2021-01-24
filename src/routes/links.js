5f59209b7ed00b86f6af4131c411c7c269711852
Eslim Daga <esliim9811@gmail.com>
2021-01-24 12:56:40 -0500

    Add list links

--- src/routes/links.js
+++ src/routes/links.js
@@ -10,7 +10,12 @@ router.post("/add", async(req,res) => {
   const { title, url, description } = req.body;
   const newLink = { title, url, description };
   await pool.query("INSERT INTO links SET ?", [newLink]);
-  res.send("Received");
+  res.redirect("/links");
+});
+
+router.get("/", async(req,res) => {
+  const links = await pool.query("SELECT * FROM links");
+  res.render("links/index", { links });
 });
 
 module.exports = router;
