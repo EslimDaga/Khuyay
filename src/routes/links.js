d71c05910acf04bc198ddcec9dcbf7c8ed10c4ac
Eslim Daga <esliim9811@gmail.com>
2021-01-24 14:18:59 -0500

    Add edit and delete routes

--- src/routes/links.js
+++ src/routes/links.js
@@ -18,4 +18,26 @@ router.get("/", async(req,res) => {
   res.render("links/index", { links });
 });
 
+router.get("/delete/:id", async(req,res) => {
+  const { id } = req.params;
+  await pool.query("DELETE FROM links WHERE ID = ?", [id]);
+  res.redirect("/links");
+});
+
+router.get("/edit/:id", async(req,res) => {
+  const { id } = req.params;
+  const links = await pool.query("SELECT * FROM links WHERE id = ?", [id]);
+  console.log(links);
+  res.render("links/edit", { links : links[0] })
+});
+
+router.post("/edit/:id", async(req,res) => {
+  const { id } = req.params;
+  const { title, description, url } = req.body;
+  const newLink = { title, description, url };
+  console.log(newLink);
+  await pool.query("UPDATE links set ? WHERE id = ?", [newLink, id]);
+  res.redirect("/links");
+});
+
 module.exports = router;
