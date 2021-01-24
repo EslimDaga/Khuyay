c00e613fd5a8f67608f6ea89eb43ddcc0a91b61d
Eslim Daga <esliim9811@gmail.com>
2021-01-23 19:10:45 -0500

    Add database connetcion

--- src/database.js
+++ src/database.js
@@ -9,16 +9,16 @@ pool.getConnection((err, connection) => {
     if (err.code === "PROTOCOL_CONNECTION_LOST") {
       console.error("Database connection was closed.");
     }
-    if (err.code === 'ER_CON_COUNT_ERROR') {
+    if (err.code === "ER_CON_COUNT_ERROR") {
       console.error("Database has to many connections");
     }
-    if (err.code === 'ECONNREFUSED') {
+    if (err.code === "ECONNREFUSED") {
       console.error("Database connection was refused");
     }
   }
 
   if (connection) connection.release();
-  console.log('DB is Connected');
+  console.log("Database is Connected");
 
   return;
 });
