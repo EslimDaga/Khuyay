0bf9caabba727366e60bc3b8617cdaef17fcb2c4
Eslim Daga <esliim9811@gmail.com>
2021-02-16 17:20:23 -0500

    Add route verses

--- src/index.js
+++ src/index.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
 app.use(require("./routes"));
 app.use(require("./routes/authentication"));
 app.use("/links", require("./routes/links"));
+app.use("/verses", require("./routes/verses"));
 
 //Public
 app.use(express.static(path.join(__dirname, "public")));
