9224db136ed33067792a5befb5b6459225937e2b
Eslim Daga <esliim9811@gmail.com>
2021-02-18 19:53:14 -0500

    Add routes get view and post edit

--- src/routes/verses.js
+++ src/routes/verses.js
@@ -33,4 +33,20 @@ router.get("/edit/:id_verse", async(req,res) => {
   res.render("verses/edit", { verses : verses[0] });
 });
 
+router.get("/view/:id_verse", async(req,res) => {
+  const { id_verse } = req.params;
+  const verses = await pool.query("SELECT * FROM verses WHERE id_verse = ?", [id_verse]);
+  console.log(verses);
+  res.render("verses/view", { verses : verses[0] });
+});
+
+router.post("/edit/:id_verse", async(req,res) => {
+  const { id_verse } = req.params;
+  const { title, description } = req.body;
+  const newVerse = { title, description };
+  req.flash("success","Verso actualizado satisfaccioriamente");
+  await pool.query("UPDATE verses set ? WHERE id_verse = ?", [newVerse, id_verse]);
+  res.redirect("/verses");
+});
+
 module.exports = router;
