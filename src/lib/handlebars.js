94338980aea23121c036d3eb5d0e333a95d80ee5
Eslim Daga <esliim9811@gmail.com>
2021-02-18 19:49:10 -0500

    Add languaje timestamp

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -2,7 +2,7 @@ const { format } = require("timeago.js");
 const helpers = {};
 
 helpers.timeago = (timestamp) => {
-  return format(timestamp);
+  return format(timestamp, "en_US");
 };
 
 module.exports = helpers;
