c0f9895b229bcb31e0d350a2c0a15a0e827440cf
Eslim Daga <esliim9811@gmail.com>
2021-02-20 18:58:22 -0500

    Add router get and post

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const pool = require("../database");
 const { isLoggedIn, isNotLoggedIn } = require("../lib/auth");
 
 router.get("/signup", isNotLoggedIn,(req,res) => {
@@ -27,6 +28,23 @@ router.post("/signin", isNotLoggedIn,(req, res, next) => {
 
 router.get("/profile", isLoggedIn,(req,res) => {
   res.render("profile");
+});
+
+router.get("/profile/edit/:id", async(req,res) => {
+  const { id } = req.params;
+  const users = await pool.query("SELECT * FROM users WHERE id = ?", [id]);
+  console.log(users);
+  res.render("users/edit", { users : users[0] });
+});
+
+router.post("/profile/edit/:id", async(req,res) => {
+  const { id } = req.params;
+  const { firstname, lastname, username, email, phone } = req.body;
+  const image = req.file.filename;
+  const newUser = { firstname, lastname, username, email, phone, image };
+  req.flash("success","Usuario actualizado satisfactoriamente");
+  await pool.query("UPDATE users set ? WHERE id = ?", [newUser, id]);
+  res.redirect("/profile");
 });
 
 router.get("/logout", isLoggedIn,(req,res) => {
