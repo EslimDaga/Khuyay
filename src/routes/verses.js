40e66ada7673f403dfb0b1177dc53d103e870b4e
Eslim Daga <esliim9811@gmail.com>
2021-02-21 16:24:04 -0500

    Add IsLoggedIn

--- src/routes/verses.js
+++ src/routes/verses.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const pool = require("../database");
 const router = express.Router();
+const pool = require("../database");
+const { isLoggedIn } = require("../lib/auth");
 
-router.get("/", async(req,res) => {
+router.get("/", isLoggedIn,async(req,res) => {
   const verses = await pool.query("SELECT * FROM verses WHERE user_id = ?", [req.user.id]);
   res.render("verses/index", { verses });
 });
 
-router.get("/add", (req,res) => {
+router.get("/add", isLoggedIn,(req,res) => {
   res.render("verses/add");
 });
 
-router.post("/add", async(req,res) => {
+router.post("/add", isLoggedIn,async(req,res) => {
   const { title, description } = req.body;
   const image = req.file.filename;
   const newVerse = { title, description, image, user_id : req.user.id };
@@ -21,21 +22,21 @@ router.post("/add", async(req,res) => {
 });
 
 
-router.get("/delete/:id_verse", async(req,res) => {
+router.get("/delete/:id_verse", isLoggedIn,async(req,res) => {
   const { id_verse } = req.params;
   await pool.query("DELETE FROM verses WHERE ID_VERSE = ?", [id_verse]);
   req.flash("success","Verso eliminado satisfactoriamente");
   res.redirect("/verses");
 });
 
-router.get("/edit/:id_verse", async(req,res) => {
+router.get("/edit/:id_verse", isLoggedIn,async(req,res) => {
   const { id_verse } = req.params;
   const verses = await pool.query("SELECT * FROM verses WHERE id_verse = ?", [id_verse]);
   console.log(verses);
   res.render("verses/edit", { verses : verses[0] });
 });
 
-router.post("/edit/:id_verse", async (req, res) => {
+router.post("/edit/:id_verse", isLoggedIn,async(req, res) => {
   const { id_verse } = req.params;
   const { title, description } = req.body;
   const image = req.file.filename;
@@ -45,7 +46,7 @@ router.post("/edit/:id_verse", async (req, res) => {
   res.redirect("/verses");
 });
 
-router.get("/view/:id_verse", async(req,res) => {
+router.get("/view/:id_verse", isLoggedIn,async(req,res) => {
   const { id_verse } = req.params;
   const verses = await pool.query("SELECT * FROM verses WHERE id_verse = ?", [id_verse]);
   console.log(verses);
